0fdd1fb072c780b77453c9be678083be9e4ef041
Mardony Lima <Mardony.Lima.cj12@gmail.com>
2025-05-19 14:41:18 -0300

    routes configured in the urls

--- DeviceMonitoringAPI/urls.py
+++ DeviceMonitoringAPI/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import IndexView
+from .views import IndexView, base
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+    path('base', base),
 ]
