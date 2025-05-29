4b1dac6257eb7dcc2bc15ae3780099664bc141bb
Mardony Lima <Mardony.Lima.cj12@gmail.com>
2025-05-28 22:19:54 -0300

    erro checkout

---
+++ DeviceMonitoringAPI/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+
+
+class SystemStatusSerializer(serializers.Serializer):
+    cpu = serializers.FloatField()
+    memory = serializers.DictField()
+    disk = serializers.DictField()
+    system_info = serializers.DictField()
