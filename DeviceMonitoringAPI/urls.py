a18a2dd68827ac3d6f46acba63d1633667e827a3
Mardony Lima <Mardony.Lima.cj12@gmail.com>
2025-05-30 18:05:22 -0300

    Test API response with Postman

--- DeviceMonitoringAPI/urls.py
+++ DeviceMonitoringAPI/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import IndexView, base
+from .views import IndexView, base, SystemStatusAPIView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('base', base),
+    path('api/system-status/', SystemStatusAPIView.as_view(), name='system-status'),
 ]
--- DeviceMonitoringAPI/views.py
+++ DeviceMonitoringAPI/views.py
@@ -1,11 +1,28 @@
 from django.shortcuts import render
 
+from django.views.generic import TemplateView
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from .services.system_monitor import collect_system_data
+from .serializers import SystemStatusSerializer
+
 # Create your views here.
 
 from django.views.generic import TemplateView
 
+
 class IndexView(TemplateView):
     template_name = 'index.html'
 
+
 def base(request):
     return render(request, 'base.html')
+
+
+class SystemStatusAPIView(APIView):
+    def get(self, request):
+        system_data = collect_system_data()
+        serializer = SystemStatusSerializer(system_data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
